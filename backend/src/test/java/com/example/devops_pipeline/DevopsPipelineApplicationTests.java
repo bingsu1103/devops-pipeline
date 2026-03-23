b025ee75dc1dbab015da6cf8080769c44f219aff
23120205 <ngogiaan11111@gmail.com>
2026-03-23 15:40:53 +0700

    first commit with spring initializr

---
+++ backend/src/test/java/com/example/devops_pipeline/DevopsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
